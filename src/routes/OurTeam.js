b0e5b1c2fb2c2ab525d5c60c59e0f21cbcff33b2
Vanshghugariya <vanshghugariya@gmail.com>
2025-10-27 10:42:05 +0530

    Initial commit

--- src/routes/OurTeam.js
+++ src/routes/OurTeam.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import TeamCard from '../components/TeamCard/TeamCard'
 import {teamdata} from './teamdata'
-import teambg from '../assets/videos/teambg.mp4'
+// import teambg from '../assets/videos/teambg.mp4'
 
 const OurTeam = () => {
   return (
